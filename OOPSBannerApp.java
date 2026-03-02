91645db11eb83f5a25d2d5013cfd9fc2aae315ba
AtreyeeB15 <atreyeebarick@gmail.com>
2026-03-02 09:40:24 +0530

    UC6-static-functions

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,27 +1,71 @@
 /**
- * OOPSBannerApp UC4 - Render OOPS as Banner using String Array and Loop
- * Goal: Improve modularity by storing banner lines in an array and using a loop.
- */
-class OOPSBannerApp {
+* OOPSBannerApp UC5 - OOPS Banner Application
+*
+* 
+* This use case extends UC5 by implementing a modular approach
+* to generate each letter's pattern through dedicated static methods.
+* This enhances code reusability and maintainability.
+.
+*
+* @author Ravichandran
+* @version 6.0
+*/
+
+public class OOPSBannerApp {
+
+    public static String[] getOPattern() {
+        return new String[] {
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
+        };
+    }
+
+
+    public static String[] getPPattern() {
+        return new String[] {
+            "**** ",
+            "*   *",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    ",
+            "*    "
+        };
+    }
+
+
+    public static String[] getSPattern() {
+        return new String[] {
+            " ****",
+            "*    ",
+            "*    ",
+            " *** ",
+            "    *",
+            "    *",
+            "**** "
+        };
+    }
+
     public static void main(String[] args) {
-        
-        // 1. Create a String array to store all seven lines of the OOPS banner
-        String[] bannerLines = new String[7];
 
-        // 2. Array Population: Construct each line using String.join()
-        // We maintain the 9-character width for each segment for proper alignment
-        bannerLines[0] = String.join(" ", "  *** ", "   *** ", "  ***** ", " **** ");
-        bannerLines[1] = String.join(" ", " *   * ", " *   * ", " *   * ", " *    ");
-        bannerLines[2] = String.join(" ", " *   * ", " *   * ", " *   * ", " *  ");
-        bannerLines[3] = String.join(" ", " *   * ", " *   * ", " ***** ", " **** ");
-        bannerLines[4] = String.join(" ", " *   * ", " *   * ", " * ", "        * ");
-        bannerLines[5] = String.join(" ", " *   * ", " *   * ", " * ", "        * ");
-        bannerLines[6] = String.join(" ", "  *** ", "   *** ", "  * ", "     **** ");
 
-        // 3. Loop Iteration: Use an Enhanced For Loop (for-each) to print
-        // This eliminates repetitive print statements and improves readability
-        for (String line : bannerLines) {
-            System.out.println(line);
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                oPattern[i] + " " +
+                oPattern[i] + " " +
+                pPattern[i] + " " +
+                sPattern[i]
+            );
         }
     }
 }
